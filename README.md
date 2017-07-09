fcfa1dbcb84656420b2f8bb8d5f312f7d347d114
bornkiller <hjj491229492@hotmail.com>
2017-07-09 16:25:10 +0800

    chore: prepare test env ready.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # typescript-lib-starter
 Starter kit with zero-config for building a library in TypeScript, featuring RollupJS, Jest, Prettier, TSLint!
+
+## Attention
+Use ES6 javascript for test suits.
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,22 @@
-{
-  "moduleFileExtensions": ["js", "json", "ts"],
-  "moduleDirectories": ["node_modules"],
-  "moduleNameMapper": {
-    "\\.(css|less|scss)$": "identity-obj-proxy"
+module.exports = {
+  transform: {
+    '\\.(ts|tsx)$': 'typescript-babel-jest',
+    '\\.(js|jsx)$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy'
   },
   // Coverage report
-  "collectCoverageFrom": ["src/**/*.js"],
-  "coverageDirectory": "coverage",
-  "coverageReporters": ["lcov", "html"]
-}
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'html'],
+  mapCoverage: true,
+  // Test configuration
+  testMatch: ['**/__tests__/**/*.spec.js'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ]
+};
