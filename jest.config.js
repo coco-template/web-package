735c97136172fa78803e4cfb00dc6d7eb43bc581
yang.yuncai <383235388@qq.com>
2018-10-31 10:04:22 +0800

    chore: use latest jest configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,13 @@
 module.exports = {
   transform: {
-    '\\.(ts|tsx|js)$': 'babel-jest',
+    '\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
-  // Coverage report, exclude develop bootstrap entry
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'html'],
-  // Test configuration
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\]'],
-  // Test configuration
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/es/'],
+  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
