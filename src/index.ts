01488de1dfdd99a74f4ec62eef66cb7fd9d8549a
huang.jian <hjj491229492@hotmail.com>
2020-09-07 14:07:24 +0800

    chore: migrate tslint to eslint

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
  * @description - @coco-platform/init-cli generated
  * @author - huang.jian <hjj491229492@hotmail.com>
  */
+// eslint-disable-next-line import/named
 export { translateOperatorSymbol, arrow } from './coco';
